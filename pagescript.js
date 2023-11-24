286dc40e7a3503c079a2a7518bb81a7ba5603486
BiteableFunds <131499753+BiteableFunds@users.noreply.github.com>
2023-11-23 20:16:21 -0500

    Update pagescript.js

--- pagescript.js
+++ pagescript.js
@@ -1,1 +1,19 @@
+// HTML Elements
+var changeamtdisplay = document.getElementById("");
 
+var add100 = document.getElementById("add100");
+var add10 = document.getElementById("add10");
+var add1 = document.getElementById("add1");
+
+var sub100 = document.getElementById("sub100");
+var sub10 = document.getElementById("sub10");
+var sub1 = document.getElementById("sub1");
+
+// Event Listeners
+add100.addEventListener();
+add10.addEventListener();
+add1.addEventListener();
+
+sub100.addEventListener();
+sub10.addEventListener();
+sub1.addEventListener();
